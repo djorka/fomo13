c09c55d7fd081da9120c08a73e3386d60e35f904
Zach Eddington <zacheddington@gmail.com>
2018-03-22 08:45:36 -0600

    Add files for code review

---
+++ Files/detail.py
@@ -0,0 +1,21 @@
+from django_mako_plus import view_function, jscontext
+from catalog import models as cmod
+
+@view_function
+def process_request(request, prodid: cmod.Product, cat_id=0):
+
+    prod = prodid
+    img = prod.image_urls()
+    cat = cmod.Category.objects.all()
+
+    if prod in request.last_five:
+        request.last_five.remove(prod)
+    request.last_five.insert(0, prod)
+
+    context = {
+        'prod': prod,
+        'img': img,
+        'cat': cat,
+    }
+
+    return request.dmp.render('detail.html', context)
